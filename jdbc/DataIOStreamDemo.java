4a49340b12d5dff465884f4c286e71dcaccff47b
MonishaM55 <54066936+MonishaM55@users.noreply.github.com>
2019-08-26 14:45:40 +0530

    Add files via upload

---
+++ jdbc/DataIOStreamDemo.java
@@ -0,0 +1,14 @@
+package com.quinnox.jdbc;
+import java.io.*;
+public class DataIOStreamDemo{
+	 public static void main(String args[])throws IOException{
+	 DataInputStream d = new DataInputStream(new FileInputStream("d:/temp/countries.txt")); 
+	DataOutputStream out = new DataOutputStream(new FileOutputStream("d:/temp/test_countries.txt")); 
+
+	String count;
+	while((count = d.readLine()) != null){ 
+		String u = count.toUpperCase(); 
+		System.out.println(u); out.writeBytes(u + " ,"); }
+	 	d.close(); 
+		out.close();  } } 
+	
